ee3b2bbd2d3f1d5a6252305f65658e2db012fdaa
Matthias Kausl <sig@segv.at>
2018-08-10 15:26:18 +0200

    created select week component

--- src/components/top.js
+++ src/components/top.js
@@ -1,13 +1,14 @@
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import InputLabel from "@material-ui/core/InputLabel";
-import { withStyles } from "@material-ui/core/styles";
-import Switch from '@material-ui/core/Switch';
-import moment from "moment";
-import { withActions, withData } from "../context/ppp";
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+import InputLabel from '@material-ui/core/InputLabel'
+import { withStyles } from '@material-ui/core/styles'
+import Switch from '@material-ui/core/Switch'
+import moment from 'moment'
+import SelectWeek from './select-week'
+import { withActions, withData } from '../context/ppp'
 
 const styles = {
   title: {
@@ -15,30 +16,27 @@ const styles = {
   },
   formControl: {
     flex: 3,
-    margin: "2px",
+    margin: '2px',
     minWidth: 120
   },
   add: {
     flex: 1,
-    textAlign: "right"
+    textAlign: 'right'
   }
-};
+}
 
-let _currentWeek = moment().format("YYYY-ww");
 
-let _hasCurrentWeek = weeks => {
-  return weeks.find(w => w === _currentWeek) != null;
-};
+
 
 let Top = props => (
   <AppBar position="static" color="default">
     <Toolbar>
       <div
         style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          width: "100%"
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%'
         }}
       >
         <Typography variant="title" className={props.classes.title}>
@@ -46,78 +44,60 @@ let Top = props => (
         </Typography>
 
 
-          <FormControl className={props.classes.formControl}>
-            <InputLabel htmlFor="week-select">Week</InputLabel>
-            <Select
-              value={props.data.selectedWeek}
-              native={true}
-              onChange={e => props.actions.selectWeek(e.target.value)}
-              inputProps={{ id: "week-select", name: "week-select" }}
-            >
-              {!_hasCurrentWeek(props.weeks) && (
-                <option key={_currentWeek} value={_currentWeek}>
-                  {moment().format("ww/YYYY")}
-                </option>
-              )}
-              {props.weeks.map(w => (
-                <option key={w} value={w}>
-                  {moment(w, "yyyy-ww").format("ww/YYYY")}
-                </option>
-              ))}
-            </Select>
-          </FormControl>
+        <SelectWeek weeks = {props.weeks}/>
 
 
-          <FormControl className={props.classes.formControl}>
-            <InputLabel htmlFor="team-select">Team</InputLabel>
-            <Select
-              value={props.data.selectedTeam}
-              native={true}
-              onChange={e =>
-                props.actions.selectTeam(
-                  e.target.value !== '-1' ? e.target.value : undefined
-                )
-              }
-              inputProps={{ id: "team-select", name: "team-select" }}
-            >
-              <option key="all" value={-1}>
-                All
+        <FormControl className={props.classes.formControl}>
+          <InputLabel htmlFor="team-select">Team</InputLabel>
+          <Select
+            value={props.data.selectedTeam}
+            native={true}
+            onChange={e =>
+              props.actions.selectTeam(
+                e.target.value !== '-1' ? e.target.value : undefined
+              )
+            }
+            inputProps={{id: 'team-select', name: 'team-select'}}
+          >
+            <option key="all" value={-1}>
+              All
+            </option>
+            {props.teams.map(w => (
+              <option key={w} value={w}>
+                {w}
               </option>
-              {props.teams.map(w => (
-                <option key={w} value={w}>
-                  {w}
-                </option>
-              ))}
-            </Select>
-          </FormControl>
+            ))}
+          </Select>
+        </FormControl>
 
-          <FormControl className={props.classes.formControl}>
-              <InputLabel htmlFor="team-select">Tags</InputLabel>
-              <Select
-                  value={props.data.selectedTag}
-                  native={true}
-                  onChange={e =>
-                      props.actions.selectTag(
-                          e.target.value !== '-1' ? e.target.value : undefined
-                      )
-                  }
-                  inputProps={{ id: "team-select", name: "team-select" }}
-              >
-                  <option key="all" value={-1}>
-                      All
-                  </option>
-                  {props.tags.map(w => (
-                      <option key={w} value={w}>
-                          {w}
-                      </option>
-                  ))}
-              </Select>
-          </FormControl>
+        <FormControl className={props.classes.formControl}>
+          <InputLabel htmlFor="team-select">Tags</InputLabel>
+          <Select
+            value={props.data.selectedTag}
+            native={true}
+            onChange={e =>
+              props.actions.selectTag(
+                e.target.value !== '-1' ? e.target.value : undefined
+              )
+            }
+            inputProps={{id: 'team-select', name: 'team-select'}}
+          >
+            <option key="all" value={-1}>
+              All
+            </option>
+            {props.tags.map(w => (
+              <option key={w} value={w}>
+                {w}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
 
-          <Switch checked={props.data.editMode} disabled={props.data.teamSelected !== undefined} onChange={props.actions.toggleEdit}/>
+        <Switch checked={props.data.editMode} disabled={props.data.teamSelected !== undefined}
+                onChange={props.actions.toggleEdit}/>
       </div>
     </Toolbar>
   </AppBar>
-);
+)
 
-export default withData(withActions(withStyles(styles)(Top)));
+export default withData(withActions(withStyles(styles)(Top)))
