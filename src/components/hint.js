bdb9d259587335a430a12caed5b4fc1ba527584a
mat <matthias.kausl@willhaben.at>
2018-11-08 10:33:36 +0100

    bold hint

--- src/components/hint.js
+++ src/components/hint.js
@@ -8,10 +8,10 @@ let Hint = props => (
     <Card style={{position: 'relative', flex: 4, minWidth: 300, margin: 10}}>
         <CardContent>
             <List dense={true}>
-                <ListItem><ListItemText primary="Focus on what you achieved (focus on results) rather than on how you did it"/></ListItem>
-                <ListItem><ListItemText primary="Use a maximum of three bullet points per P (be concise)"/></ListItem>
-                <ListItem><ListItemText primary="Assume little to no detailed understanding of what you do"/></ListItem>
-                <ListItem><ListItemText primary="Avoid jargon and abbreviations"/></ListItem>
+                <ListItem><ListItemText primaryTypographyProps={{style:{fontWeight: 'bold'}}} primary="Focus on what you achieved (focus on results) rather than on how you did it"/></ListItem>
+                <ListItem><ListItemText primaryTypographyProps={{style:{fontWeight: 'bold'}}} primary="Use a maximum of three bullet points per P (be concise)"/></ListItem>
+                <ListItem><ListItemText primaryTypographyProps={{style:{fontWeight: 'bold'}}} primary="Assume little to no detailed understanding of what you do"/></ListItem>
+                <ListItem><ListItemText primaryTypographyProps={{style:{fontWeight: 'bold'}}} primary="Avoid jargon and abbreviations"/></ListItem>
             </List>
         </CardContent>
     </Card>
