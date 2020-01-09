37cdeb9b35ed62f0dd3c110e787fe7422cee3168
Matthias Kausl <matthias.kausl@willhaben.at>
2020-01-09 09:22:07 +0100

    fix date

--- src/components/select-week.js
+++ src/components/select-week.js
@@ -30,13 +30,15 @@ let select = props => <div>
         {moment().add(5,'d').format("D.M.)")}
       </option>
     )}
-    {props.weeks.map(w => (
+    {props.weeks.map(w => {
+      console.log(moment(w, "YYYY-ww").format())
+      return (
       <option key={w} value={w}>
-        {moment(w, "yyyy-ww").format("ww/YYYY")}
-        {moment(w, "yyyy-ww").add(1,'d').format(" (D.M. - ")}
-        {moment(w, "yyyy-ww").add(5,'d').format("D.M.)")}
+        {moment(w, "YYYY-ww").format("ww/YYYY")}
+        {moment(w, "YYYY-ww").add(1,'d').format(" (D.M. - ")}
+        {moment(w, "YYYY-ww").add(5,'d').format("D.M.)")}
       </option>
-    ))}
+    )})}
   </Select>
 </FormControl>
 </div>
